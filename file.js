e5ab2f36c696017cf9c691ef8f01be9a0aab21f9
Alexandria201 <aov6lt@ukr.net>
2022-01-29 15:06:46 +0200

    Implement the following chains with inheritances

---
+++ file.js
@@ -0,0 +1,57 @@
+function Organism(){
+}
+Organism.prototype.isAlive = function(){
+  if (this.age < 110){
+    return true
+  } else {
+    return false
+  }
+}
+
+function Creater(){
+}
+Creater.prototype = Object.create(Organism.prototype)
+Creater.prototype.constructor = Creater;
+Creater.prototype.getAge = function(){
+  return this.age
+}
+
+function Persone(){
+}
+Persone.prototype = Object.create(Creater.prototype)
+Persone.prototype.constructor = Persone;
+Persone.prototype.sayHi = function(){
+  return `Hello ${this.name}`;
+}
+
+const Boy = function(options){
+  this.name = options.name;
+  this.age = options.age;
+  this.color = options.color;
+}
+
+Boy.prototype = Object.create(Persone.prototype)
+Boy.prototype.constructor = Boy;
+Boy.prototype.getFavoriteColor = function(){
+  return this.color
+}
+
+
+const john = new Boy({name:'John',age: 33,color:'blue'})
+
+console.log(john)
+
+const Girl = function(options){
+  this.name = options.name;
+  this.age = options.age;
+  this.color = options.color;
+}
+
+Girl.prototype = Object.create(Persone.prototype);
+Girl.prototype.constructor = Girl;
+Girl.prototype.getFavoriteColor = function(){
+  return this.color
+}
+
+const Bella = new Girl({name:'Bella', age:12, color:'green'})
+console.log(Bella)
